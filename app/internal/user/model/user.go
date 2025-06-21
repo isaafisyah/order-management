ac3261ee62b3b4c7fdf46f642672070bfca3115f
Isa Afisyah <isaafisyah@gmail.com>
2025-06-21 19:40:23 +0700

    add filed createdAt in model user

--- app/internal/user/model/user.go
+++ app/internal/user/model/user.go
@@ -5,4 +5,5 @@ type User struct {
 	Name     string `gorm:"size:100;not null"`
 	Email    string `gorm:"size:100;not null;uniqueIndex"`
 	Password string `gorm:"size:255;not null;"`
+	CreatedAt string `gorm:"autoCreateTime"`
 }
--- app/internal/user/response/user_response.go
+++ app/internal/user/response/user_response.go
@@ -6,6 +6,7 @@ type UserResponse struct {
 	ID    int    `json:"id"`
 	Name  string `json:"name"`
 	Email string `json:"email"`
+	CreatedAt string `json:"created_at"`
 }
 
 func ToUserResponse(user model.User) UserResponse {
@@ -13,6 +14,7 @@ func ToUserResponse(user model.User) UserResponse {
 		ID:    user.ID,
 		Name:  user.Name,
 		Email: user.Email,
+		CreatedAt: user.CreatedAt,
 	}
 }
 
