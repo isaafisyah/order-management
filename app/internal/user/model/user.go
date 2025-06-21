5c3897fd6ab2e8423bc5ef6ab87e95657f7f11b3
Isa Afisyah <isaafisyah@gmail.com>
2025-06-21 19:37:18 +0700

    minor user model

--- app/internal/user/model/user.go
+++ app/internal/user/model/user.go
@@ -2,7 +2,7 @@ package model
 
 type User struct {
 	ID       int `gorm:"primary_key;auto_increment"`
-	Name     string `gorm:"size:100;not null" json:"name"`
-	Email    string `gorm:"size:100;not null;uniqueIndex" json:"email"`
-	Password string `gorm:"size:255;not null;" json:"password"`
+	Name     string `gorm:"size:100;not null"`
+	Email    string `gorm:"size:100;not null;uniqueIndex"`
+	Password string `gorm:"size:255;not null;"`
 }
