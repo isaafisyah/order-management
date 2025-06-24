d9d1d436a898dfc1cf95864df020907ec08d4e56
Isa Afisyah <isaafisyah@gmail.com>
2025-06-24 08:56:48 +0700

    enhance validator

--- app/utils/validator/validator.go
+++ app/utils/validator/validator.go
@@ -1,9 +1,28 @@
 package validator
 
-import "github.com/go-playground/validator/v10"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
 
 var validate = validator.New()
 
 func ValidateStruct(s interface{}) error {
-	return validate.Struct(s)
+	err := validate.Struct(s)
+	if err == nil {
+		return nil
+	}
+
+	if validationErrors, ok := err.(validator.ValidationErrors); ok {
+		var messages []string
+		for _, e := range validationErrors {
+			// Ubah format error jadi lebih user friendly
+			messages = append(messages, fmt.Sprintf("Field %s is %s", e.Field(), e.Tag()))
+		}
+		return fmt.Errorf(strings.Join(messages, ", "))
+	}
+
+	return err
 }
